cab996a8e46988ecb9c3f31e02af8e883c2fb6f6
JLChnToZ <jeremylam619@gmail.com>
2020-08-21 02:28:06 +0800

    Add puzzle size to certificate

--- src/certificate.ts
+++ src/certificate.ts
@@ -5,7 +5,7 @@ const certificate = document.querySelector('#certificate')!;
 const timeStartElement = certificate.querySelector<SVGTextElement>('text#time-start')!;
 const timeEndElement = certificate.querySelector<SVGTextElement>('text#time-end')!;
 const timeUsedElement = certificate.querySelector<SVGTextElement>('text#time-used')!;
-
+const puzzleSizeElement = certificate.querySelector<SVGTextElement>('test#puzzle-size')!;
 
 export function showCertificate(mainHandler: MainHandler) {
   if(!certificate) return;
@@ -15,11 +15,13 @@ export function showCertificate(mainHandler: MainHandler) {
   timeStartElement.textContent = mainHandler.startTime?.toString() ?? '';
   timeEndElement.textContent = mainHandler.endTime?.toString() ?? '';
   timeUsedElement.textContent = formatTime(mainHandler.time);
+  puzzleSizeElement.textContent = `${mainHandler.xCount}×${mainHandler.yCount} (${mainHandler.xCount * mainHandler.yCount}Pieces)`;
 }
 
 export function hideCetificate() {
   certificate?.classList.remove('show');
   timeStartElement.textContent = '';
   timeEndElement.textContent = '';
-  timeUsedElement.textContent = '--:--';
+  timeUsedElement.textContent = '';
+  puzzleSizeElement.textContent = '';
 }
